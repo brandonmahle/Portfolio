117952529dc2f0e3965b9389de2ee1b801bc2da6
Brandon <95604207+brandonmahle@users.noreply.github.com>
2023-11-12 18:44:07 -0800

    Add files via upload

--- Python/Python-SQL-MovieWatchlist/database.py
+++ Python/Python-SQL-MovieWatchlist/database.py
@@ -1,16 +1,85 @@
-import sqlite3
-connection = sqlite3.connect("data.db")
-connection.row_factory = sqlite3.Row
+# title, release_date, watched
+
+import datetime #import dates
+import sqlite3 #import database
+
+CREATE_MOVIES_TABLE = """ 
+CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    release_timestamp REAL
+);"""
+
+CREATE_USERS_TABLE = """ 
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY
+);"""
+
+CREATE_WATCHED_TABLE = """ 
+CREATE TABLE IF NOT EXISTS watched (
+    user_username TEXT,
+    movie_id INTEGER,
+    FOREIGN KEY(user_username) REFERENCES users(username),
+    FOREIGN KEY(movie_id) REFERENCES movie(id)
+);"""
+
+INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp) VALUES (?, ?);"
+INSERT_USER = "INSERT INTO users (username) VALUES (?);"
+DELETE_MOVIE = "DELETE FROM movies WHERE title = ?;"
+SELECT_ALL_MOVIES = "SELECT * FROM movies;"
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+SELECT_WATCHED_MOVIES = """
+SELECT movies.* FROM movies
+JOIN watched ON movies.id = watched.movie_id
+JOIN users ON users.username = watched.user_username
+WHERE users.username = ?
+ORDER BY movies.id ASC;"""
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?)"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+SEARCH_MOVIES = "SELECT * FROM movies where title like ?;"
+CREATE_RELEASE_INDEX = "CREATE INDEX IF NOT EXISTS idx_movies_release ON movies (release_timestamp);"
+
+
+
+connection = sqlite3.connect("moviedata.db")
 
 def create_table():
+    with connection: 
+        connection.execute(CREATE_MOVIES_TABLE)
+        connection.execute(CREATE_USERS_TABLE) # this must be created second because of the foreign keys in watched
+        connection.execute(CREATE_WATCHED_TABLE)
+        connection.execute(CREATE_RELEASE_INDEX)
+        
+def add_user(username):
     with connection:
-        connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
+        connection.execute(INSERT_USER, (username,))
 
-def add_entry(entry_content, entry_date):
+def add_movie(title, release_timestamp):
+    with connection: 
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+
+def get_movies(upcoming = False):
     with connection:
-        connection.execute("INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date))
+        cursor = connection.cursor() #could also do cursor = connection.execute(SELECT_ALL_MOVIES)
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES) #could also do cursor = connection.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
 
-def get_entries():
-    cursor = connection.execute("SELECT * FROM entries;")
-    return cursor
+def watch_movie (username, movie_id):
+    with connection:
+        connection.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
 
+def get_watched_movies(username):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES, (username,))
+        return cursor.fetchall()
+    
+def search_movies(search_term):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIES, (f"%{search_term}%",))
+        return cursor.fetchall()
